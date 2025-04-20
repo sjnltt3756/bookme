7305c75ceccd2268f84c84060bf7a0e89769eaf8
sjnltt3756 <sjnltt3756@gmail.com>
2025-04-20 11:30:01 +0900

    Initial commit

---
+++ src/main/java/com/bookme/BookmeApplication.java
@@ -0,0 +1,13 @@
+package com.bookme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookme/BookmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
