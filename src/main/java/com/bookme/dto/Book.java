8eb8e6bff1454fd18fda3ec160701f2fdc7df179
sjnltt3756 <sjnltt3756@gmail.com>
2025-04-22 20:05:53 +0900

    썸네일 링크 추가

--- src/main/java/com/bookme/dto/Book.java
+++ src/main/java/com/bookme/dto/Book.java
@@ -11,10 +11,12 @@ public class Book {
     private String title;
     private List<String> authors;
     private String thumbnail;
+    private String infoLink;
 
-    public Book(String title, List<String> authors, String thumbnail) {
+    public Book(String title, List<String> authors, String thumbnail, String infoLink) {
         this.title = title;
         this.authors = authors;
         this.thumbnail = thumbnail;
+        this.infoLink = infoLink;
     }
 }
